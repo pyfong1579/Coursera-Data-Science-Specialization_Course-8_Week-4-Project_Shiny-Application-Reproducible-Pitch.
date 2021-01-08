6144f29818cae0a04866561e42050259f0423d91
P.Y.Fong <pyfong1@gmail.com>
2021-01-08 20:36:59 +0800

    Final Commit: All file tweaked and reviewed ready for submission.

--- IrisSelector/ui.R
+++ IrisSelector/ui.R
@@ -3,12 +3,18 @@ library(DT) # A Wrapper of the JavaScript Library 'DataTables'
 
 fluidPage(
     titlePanel(strong("Iris Selector")),
+    
     #Documentation and how to use.
     h5(strong("Documentation")),
     p("This app is a simple Iris Selector which tables the Iris dataset from R,
       based on Species selection using a highly 'sophisticated algorithmn'."),
     p(em("To use the selector, simply choose species from the drop down or key
          a search term in the search box.")),
+    p("The Code available at",
+      a(href="http://github.com/pyfong1579/Coursera-Data-Science-Specialization_Course-8_Week-4-Project_Shiny-Application-Reproducible-Pitch.",
+        "Iris Selector 2021")),
+    p("The pitch is available at",
+      a(href="http://rpubs.com/pyfong/711002", "Iris Pitch")),
     
     # Create a Row for selectInputs
     fluidRow(
